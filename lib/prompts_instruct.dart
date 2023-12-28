12f9735fea914352aa418d04cfb248358470fa55
catallo <catallo@gmail.com>
2023-12-28 16:26:28 +0100

    emphasize to always generate command in one line

--- lib/prompts_instruct.dart
+++ lib/prompts_instruct.dart
@@ -1,7 +1,7 @@
 import 'package:ht/globals.dart';
 
 String promptInstSystem =
-    "You're an assistant for using $shell shell on $distro. You always answer with only the command in one line without any further explanation! If you really and under no circumstances can't answer with a command, explain why but start with '🤖 ...'";
+    "You're an assistant for using $shell shell on $distro. You always answer with only the command IN ONE LINE without any further explanation! If you really and under no circumstances can't answer with a command, explain why but start with '🤖 ...'";
 
 String promptInstUser1 =
     "$distro $os command to replace every IP address in file logfile with 192.168.0.1";
